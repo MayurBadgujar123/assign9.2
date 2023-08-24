d37d0284c13b8cca84d287f163701457f86da3c7
MayurBadgujar123 <132994310+MayurBadgujar123@users.noreply.github.com>
2023-08-24 22:10:26 +0530

    Add files via upload

---
+++ Employee/Employee/Employee.cs
@@ -0,0 +1,105 @@
+﻿namespace  System;
+
+internal  class Employee
+{
+    private string name;
+    private int empNo;
+    private decimal basic;
+    private short deptNo;
+
+    public string Name
+    {
+        get { return name; }
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Name cannot be blank or empty.");
+            name = value;
+        }
+    }
+    public int EmpNo
+    {
+        get { return empNo; }
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("EmpNo must be greater than 0.");
+            empNo = value;
+        }
+    }
+    public decimal Basic
+    {
+        get { return basic; }
+        set
+        {
+            // Set the range for the Basic property as per your requirement
+            if (value < 0 || value > 10000)
+                throw new ArgumentException("Basic must be between the specified range.");
+            basic = value;
+        }
+    }
+    public short DeptNo
+    {
+        get { return deptNo; }
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("DeptNo must be greater than 0.");
+            deptNo = value;
+        }
+    }
+
+    public Employee(int empNo, string name, decimal basic, short deptNo)
+    {
+        EmpNo = empNo;
+        Name = name;
+        Basic = basic;
+        DeptNo = deptNo;
+    }
+
+    public Employee(int empNo, string name, decimal basic)
+        : this(empNo, name, basic, 0)
+    {
+    }
+
+    public Employee(int empNo, string name)
+        : this(empNo, name, 0, 0)
+    {
+    }
+
+    public Employee(int empNo)
+        : this(empNo, "", 0, 0)
+    {
+    }
+
+    public Employee()
+        : this(0, "", 0, 0)
+    {
+    }
+
+    public decimal GetNetSalary()
+    {
+        // Implement your net salary calculation logic here
+        // For example:
+        decimal netSalary = Basic * 0.8M; // 80% of the basic salary
+        return netSalary;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Employee o1 = new Employee(1, "Amol", 123465, 10);
+        Employee o2 = new Employee(1, "Amol", 123465);
+        Employee o3 = new Employee(1, "Amol");
+        Employee o4 = new Employee(1);
+        Employee o5 = new Employee();
+
+        Console.WriteLine("o1 EmpNo: " + o1.EmpNo);
+        Console.WriteLine("o2 EmpNo: " + o2.EmpNo);
+        Console.WriteLine("o3 EmpNo: " + o3.EmpNo);
+        Console.WriteLine("o4 EmpNo: " + o4.EmpNo);
+        Console.WriteLine("o5 EmpNo: " + o5.EmpNo);
+    }
+}
---
+++ InheritanceExamples1/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace InheritanceExamples1
+{
+    internal class Program
+    {
+        static void Main1()
+        {
+            BaseClass o1 = new BaseClass();
+            //o1.
+
+            DerivedClass o2 = new DerivedClass();
+            //o2.
+        }
+    }
+
+    public class BaseClass //: Object
+    {
+        public int i;
+    }
+
+    public class DerivedClass : BaseClass
+    {
+        public int j;
+    }
+}
+
+//access specifiers
+namespace InheritanceExamples2
+{
+    class Program
+    {
+        static void Main2()
+        {
+            //BaseClass o = new BaseClass();
+            //o.
+
+            //TestAccessSpecifiers.BaseClass o2 = new TestAccessSpecifiers.BaseClass();
+            //o2.
+        }
+    }
+    //public -> everywhere
+    //private -> same class
+    //protected -> same class, derived class
+    //internal -> same class, same assembly(same project)
+    //protected internal -> same class, derived class, same assembly
+    //private protected - same class, derived class which is in the same assembly
+    public class BaseClass
+    {
+        public int PUBLIC;
+        private int PRIVATE;
+        protected int PROTECTED;
+        internal int INTERNAL;
+        protected internal int PROTECTED_INTERNAL;
+        private protected int PRIVATE_PROTECTED;
+        int x; //private
+
+    }
+    public class DerivedClass : TestAccessSpecifiers.BaseClass    //:  BaseClass
+    {
+
+        void DoNothing()
+        {
+            //this.
+        }
+    }
+}
+
+
+//constructors in inheritance
+namespace InheritanceExamples3
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            //DerivedClass o = new DerivedClass();
+
+            DerivedClass o2 = new DerivedClass(123, 456);
+        }
+    }
+    public class BaseClass
+    {
+        public int i;
+        public BaseClass()
+        {
+            Console.WriteLine("base class no param cons");
+            i = 10;
+        }
+        public BaseClass(int i)
+        {
+            Console.WriteLine("base class int cons");
+            this.i = i;
+        }
+    }
+    public class DerivedClass : BaseClass
+    {
+        public int j;
+        public DerivedClass()
+        {
+            Console.WriteLine("derived class no param cons");
+            //i = 10;
+            j = 20;
+        }
+        public DerivedClass(int i, int j) : base(i)
+        {
+            Console.WriteLine("derived class int,int cons");
+            //this.i = i;
+            this.j = j;
+        }
+    }
+}
